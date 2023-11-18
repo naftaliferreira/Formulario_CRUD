50bc17872997855bea50e1bca74407f7b01c810a
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-18 19:09:47 -0300

    correção e adição de frames na interface
    correção de tabelas no banco de dados, e criação de frames na interface

--- banco.py
+++ banco.py
@@ -8,5 +8,5 @@ con = lite.connect("dados.db")
 with con:
     cur = con.cursor()
     cur.execute(
-        "CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE, estado TEXT, assunto TEXT)"
+        "CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE, estado TEXT, sobre TEXT)"
     )
---
+++ main.py
@@ -0,0 +1,36 @@
+# importando o tkinter
+from tkinter import *
+
+################# cores ###############
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"  # letra
+co5 = "#e06636"  # - profit
+co6 = "#038cfc"  # azul
+co7 = "#ef5350"  # vermelha
+co8 = "#263238"  # + verde
+co9 = "#e9edf5"  # sky blue
+
+
+# criando janela
+
+janela = Tk()
+janela.title("")
+janela.geometry("1043x453")
+janela.configure(bg=co9)
+janela.resizable(
+    width=False, height=False
+)  # impede que a janela tenha o seu tamanho reajustado com o mouse
+
+# dividindo a janela
+
+frame_cima = Frame(janela, width=310, height=50, bg=co2, relief="flat")
+frame_cima.grid(row=0, column=0)
+
+frame_baixo = Frame(janela, width=310, height=403, bg=co1, relief="flat")
+frame_baixo.grid(row=1, column=0)
+
+
+janela.mainloop()
