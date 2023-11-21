268532d2d1a719f3d53b6d1d31dd606bb20d0d76
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-21 17:41:31 -0300

    Função Create
    Conclusão da função inserir informações

--- view.py
+++ view.py
@@ -2,5 +2,27 @@
 import sqlite3 as lite
 
 
+# CRUD
+
+# Create = Inserir / Criar
+
+# Read = Acessar / Mostrar
+
+# Update = Atualizar
+
+# Delete = Excluir / Apagar
+
 # criando conexão
 con = lite.connect("dados.db")
+
+
+lista = ['Joao Futi','joao@mail.com', 123456789, "12/19/2010", 'Normal', 'gostaria de consultar pessoalmente']
+
+# Função inserir informações
+with con:
+    cur = con.cursor()
+    query = "INSERT INTO formulario (nome, email, telefone, dia_em, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
+    cur.execute(query, lista)
+
+# Função acessar informações 
+
