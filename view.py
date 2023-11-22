1a87c0e97a0567c43cce138de1ff087181fe0f70
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-21 23:34:38 -0300

    teste função read no bd
    teste na função de inserir informações ao banco de dados.

--- view.py
+++ view.py
@@ -24,14 +24,17 @@ lista = [
     "Normal",
     "gostaria de consultar pessoalmente",
 ]
-
+"""
 # Função inserir informações
 with con:
     cur = con.cursor()
     query = "INSERT INTO formulario (nome, email, telefone, dia_em, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
     cur.execute(query, lista)
-
+"""
 # Função acessar informações
 with con:
     cur = con.cursor()
-    cur.execute(query, lista)
+    query = "SELECT * FROM formulario"
+    cur.execute(query)
+    info = cur.fetchall()
+    print(info)
