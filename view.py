51e7c014b86e46f332d890f37ea70d55e7a22659
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-21 23:15:50 -0300

    Update view.py

--- view.py
+++ view.py
@@ -16,7 +16,14 @@ import sqlite3 as lite
 con = lite.connect("dados.db")
 
 
-lista = ['Joao Futi','joao@mail.com', 123456789, "12/19/2010", 'Normal', 'gostaria de consultar pessoalmente']
+lista = [
+    "Joao Futi",
+    "joao@mail.com",
+    123456789,
+    "12/19/2010",
+    "Normal",
+    "gostaria de consultar pessoalmente",
+]
 
 # Função inserir informações
 with con:
@@ -24,5 +31,7 @@ with con:
     query = "INSERT INTO formulario (nome, email, telefone, dia_em, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
     cur.execute(query, lista)
 
-# Função acessar informações 
-
+# Função acessar informações
+with con:
+    cur = con.cursor()
+    cur.execute(query, lista)
