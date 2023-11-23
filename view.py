e9ff99364eaea3368fbc56ba0bac354011ea13ee
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-11-23 20:14:11 -0300

    Adição da função inserir
    Função inserir adicionada e testada com sucesso

--- view.py
+++ view.py
@@ -15,23 +15,13 @@ import sqlite3 as lite
 # criando conexão
 con = lite.connect("dados.db")
 
-"""
-lista = [
-    "Joao Futi",
-    "joao@mail.com",
-    123456789,
-    "12/19/2010",
-    "Normal",
-    "gostaria de consultar pessoalmente",
-]
 
 # Função inserir informações
-with con:
-    cur = con.cursor()
-    query = "INSERT INTO formulario (nome, email, telefone, dia_em, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
-    cur.execute(query, lista)
-
-"""
+def inserir_info(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO formulario (nome, email, telefone, dia_em, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
+        cur.execute(query, i)
 
 
 # Função acessar informações
