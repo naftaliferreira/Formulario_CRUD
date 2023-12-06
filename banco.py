0f0a3bb164755a0e881b89e63f5f7381e13cf62a
naftaliferreira <naftalibarbosaferreira@gmail.com>
2023-12-05 22:10:04 -0300

    Correções ao bd
    Alteração do nome do bd para form ao inves de dados, deixando o projeto mais claro de ser lido. (não recomendado alterar)

--- banco.py
+++ banco.py
@@ -2,11 +2,11 @@
 import sqlite3 as lite
 
 # criando conexão
-con = lite.connect("dados.db")
+con = lite.connect("form.db")
 
 # criando tabela
 with con:
     cur = con.cursor()
     cur.execute(
-        "CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE, estado TEXT, assunto TEXT)"
+        "CREATE TABLE Formulario(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT, email TEXT, telefone TEXT,  dia_em DATE, estado TEXT, assunto TEXT)"
     )
